c869efaf4ed211067e9eb3ea5474b21fbbee9d93
Luis-Mocha <luismocha97@gmail.com>
2023-06-14 16:35:38 +0200

    creo pagina comics.show

---
+++ resources/views/pages/comics/show.blade.php
@@ -0,0 +1,19 @@
+@extends( 'layout.app' )
+
+@section ('title-page')
+DC-Comics | Comic
+@endsection
+
+@section( 'content-main' )
+
+    <div id="comics-main" class="container">
+
+        
+
+        <div class="card-container row my-2">
+            heloooo
+        </div>
+        
+    </div>
+
+@endsection
