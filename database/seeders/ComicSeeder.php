cc6f2c2daf8db35ae3c644831ded107ef1c8ba87
Luis-Mocha <luismocha97@gmail.com>
2023-06-14 15:53:04 +0200

    implemento il modello

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+// Implemento il modello Comic
+use App\Models\Comic;
+
 class ComicSeeder extends Seeder
 {
     /**
