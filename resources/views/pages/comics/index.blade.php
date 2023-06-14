587f094335cc5c87d8af786106d357dfff5200d8
Luis-Mocha <luismocha97@gmail.com>
2023-06-14 17:35:32 +0200

    route to comics.show

--- resources/views/pages/comics/index.blade.php
+++ resources/views/pages/comics/index.blade.php
@@ -18,10 +18,10 @@ DC-Comics | Comics
     
             <div class="comic-card col-6 col-md-4 col-lg-2 g-5 d-flex flex-column align-items-center justify-content-between text-center">
 
-                <a href="{{ route('comics.show', ['comic' => $elem->id]) }}">
+                <a href="{{ route('comics.show' , [ 'comic' => $elem->id ] ) }}">
 
                     <img src=" {{ $elem['thumb'] }} " alt="Thumb">
-                    
+
                 </a>
                 
                 <div class="cardTitle">{{$elem['title']}}</div>
