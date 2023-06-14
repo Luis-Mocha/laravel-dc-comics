2e5c9801279dd3f9361d2c16be46461cfd63f7ae
Luis-Mocha <luismocha97@gmail.com>
2023-06-14 15:25:24 +0200

    link a comics.index

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,8 +9,7 @@ DC-Comics | Home
     <div class="text-center p-5">
         <h1>Pagina Home</h1>
         <p>Still working on it. But try the
-            {{-- <a href="{{ route('comics') }}" class="text-primary text-decoration-underline">Comics Page</a> --}}
-            <a href="">Comics Page</a>
+            <a href="{{ route('comics.index') }}" class="text-primary text-decoration-underline">Comics Page</a>
         </p>
     </div>
     
