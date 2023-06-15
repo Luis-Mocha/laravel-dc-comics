581e89ce87fa3272ed1f694acfb732359d6df66e
Luis-Mocha <luismocha97@gmail.com>
2023-06-15 18:00:57 +0200

    modifico form

--- resources/views/pages/comics/create.blade.php
+++ resources/views/pages/comics/create.blade.php
@@ -16,13 +16,13 @@ DC-Comics | Comic Create
 
         <h1 class="text-capitalize">Add your Comic</h1>
 
-        <form action=" {{ route('comics.store') }} " method="POST" class="row">
+        <form action=" {{ route('comics.store') }} " method="POST" class="row" autocomplete="off">
 
             @csrf
 
             <div class="form-group mt-3">
                 <label for="input-title" class="form-label">Title:</label>
-                <input type="text" id="input-title" class="form-control" name="title" placeholder="Inserisci il titolo" autofocus> 
+                <input type="text" id="input-title" class="form-control" name="title" placeholder="Inserisci il titolo" autofocus required> 
             </div>
 
             <div class="form-group mt-3">
@@ -33,27 +33,27 @@ DC-Comics | Comic Create
 
             <div class="form-group mt-3">
                 <label for="input-thumb" class="form-label">Thumb:</label>
-                <input type="text" id="input-thumb" class="form-control" name="thumb" placeholder="Inserisci il link alla copertina"> 
+                <input type="text" id="input-thumb" class="form-control" name="thumb" placeholder="Inserisci il link alla copertina" required> 
             </div>
 
-            <div class="form-group mt-3 col-6">
+            <div class="form-grp mt-3 col-6">
                 <label for="input-price" class="form-label">Price:</label>
-                <input type="text" id="input-price" class="form-control" name="price" placeholder="Inserisci il prezzo"> 
+                <input type="number" name="price" id="input-price" class="form-control" placeholder="Inserisci il prezzo" min="0" max="100" required>
             </div>
 
             <div class="form-group mt-3 col-6">
                 <label for="input-series" class="form-label">Series:</label>
-                <input type="text" id="input-series" class="form-control" name="series" placeholder="Inserisci la serie di appartenenza"> 
+                <input type="text" id="input-series" class="form-control" name="series" placeholder="Inserisci la serie di appartenenza" required> 
             </div>
 
             <div class="form-group mt-3 col-6">
                 <label for="input-sale_date" class="form-label">Sale Date:</label>
-                <input type="date" id="input-sale_date" class="form-control" name="sale_date"> 
+                <input type="date" id="input-sale_date" class="form-control" name="sale_date" required> 
             </div>
 
             <div class="form-group mt-3 col-6">
                 <label for="input-type" class="form-label">Type:</label>
-                <input type="text" id="input-type" class="form-control" name="type" placeholder="Inserisci la tipologia"> 
+                <input type="text" id="input-type" class="form-control" name="type" placeholder="Inserisci la tipologia" required> 
             </div>
 
             <button type="submit" class="btn btn-primary my-4 col-2 mx-auto">Add Comic !</button>
